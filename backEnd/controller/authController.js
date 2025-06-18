bce5d9a08d9dc9c275e2d978d5d97c8ff7b460e1
Mohamed Irfan <irfan@Mohameds-MacBook-Air.local>
2025-06-19 00:06:07 +0530

    Initial push: frontend and backend setup

--- backEnd/controller/authController.js
+++ backEnd/controller/authController.js
@@ -53,6 +53,6 @@ exports.loginUser = async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error1' });
   }
 };
